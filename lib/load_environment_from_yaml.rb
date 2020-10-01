e9ae4803db7355936649ff61829e1f8c0985da9b
Tony Bowden <tony@tmtm.com>
2020-10-01 11:15:30 +0100

    Load PHH_BOT flag from secrets.yml
    If this is set, edits will be made with a bot flag set.

--- lib/load_environment_from_yaml.rb
+++ lib/load_environment_from_yaml.rb
@@ -4,6 +4,7 @@ begin
   secrets = YAML.load_file(File.expand_path(__dir__ + '/../secrets.yml'))
   ENV['WIKI_USERNAME'] = secrets['WIKI_USERNAME']
   ENV['WIKI_PASSWORD'] = secrets['WIKI_PASSWORD']
+  ENV['PHH_BOT']       = secrets['PHH_BOT'].to_s
 rescue Errno::ENOENT
   abort "Please run 'cp secrets.yml-example secrets.yml' and then fill out secrets.yml"
 end
